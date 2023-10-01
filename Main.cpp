59fed223583169c984584ba1e7b38d0d9f6e86b0
venkateshrajan <venkatesh.xaviers@gmail.com>
2023-10-01 17:04:45 +0530

    Added gtest

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,7 @@
 
 #include "absl/strings/str_join.h"
 #include "GitExtended.h"
-#include <string>
-#include <vector>
+#include <linux/limits.h>
 
 int main() {
   CGitExtended git;
@@ -13,6 +12,14 @@ int main() {
   std::string s = absl::StrJoin(v, "-");
 
   std::cout << "Joined string: " << s << std:: endl;
+  
+  std::FILE *fp = popen("ls", "r");
+  if (fp == NULL)
+    return 1;
+
+  char path[PATH_MAX] = { 0 };
+  while (fgets(path, PATH_MAX, fp) != NULL)
+    std::cout << path;
 
   return 0;
 }
