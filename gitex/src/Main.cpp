2bd2613e773906191d4a595d8e0e946890056e1c
venkateshrajan <venkatesh.xaviers@gmail.com>
2023-10-02 14:30:58 +0530

    Added boost dependency and changed the project structure

--- gitex/src/Main.cpp
+++ gitex/src/Main.cpp
@@ -1,8 +1,11 @@
 #include "pch.h"
 
 #include "absl/strings/str_join.h"
+#include "boost/process.hpp"
 #include "GitExtended.h"
 #include "utilities.h"
+
+using namespace boost::process;
 
 int main() {
   CGitExtended git;
@@ -12,7 +15,16 @@ int main() {
   std::string s = absl::StrJoin(v, "-");
 
   std::cout << "Joined string: " << s << std:: endl;
+
+  ipstream pipe_stream;
+  child c("gcc --version", std_out > pipe_stream);
+
+  std::string line;
+  while (pipe_stream && std::getline(pipe_stream, line) && !line.empty())
+    std::cerr << line << std::endl;
   
+  c.wait();
+
   return 0;
 }
 
